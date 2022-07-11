a9416268646cc3198dd79c1d339649e37c1b6102
Sanjana Ganguly <sanjanaganguly15@gmail.com>
2022-07-12 02:08:21 +0530

    Sorting-Unique Keys

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -8,12 +8,16 @@ export class TableBody extends Component {
         return _.get(item, column.path)
     }
 
+    createKey = (item, column) => {
+        return item._id + (column.path || column.key)
+    }
+
     render() {
         const { data, columns } = this.props;
         return (
             <tbody>
                 {data.map(item => (
-                    <tr>{columns.map(col => <td>{this.renderCell(item, col)}</td>)}</tr>
+                    <tr key={item._id}>{columns.map(col => <td key={this.createKey(item, col)}>{this.renderCell(item, col)}</td>)}</tr>
                 ))}
             </tbody>
         )
