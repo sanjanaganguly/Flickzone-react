98c8976419950054b1a5b252afed6358a72b51bf
Sanjana Ganguly <sanjanaganguly15@gmail.com>
2022-08-03 01:25:52 +0530

    Adding the NavBar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
-import "./App.css";
-import { Route, Routes, Redirect, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
-import { dom } from "@fortawesome/fontawesome-svg-core";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
+      <><Navbar /><main className="container">
         <Routes>
           <Route path="/movies" element={<Movies />}></Route>
           <Route path="/customers" element={<Customers />}></Route>
           <Route path="/rentals" element={<Rentals />}></Route>
           <Route path="/not-found" element={<NotFound />}></Route>
-          <Route path="/" element={<Navigate replace to = "/movies"/>}></Route>
-          <Route path="*" element={<Navigate replace to ="/not-found"/>} />
+          <Route path="/" element={<Navigate replace to="/movies" />}></Route>
+          <Route path="*" element={<Navigate replace to="/not-found" />} />
         </Routes>
-      </main>
+      </main></>
     );
   }
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import { React } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        // Link - when we use <NavLink> tag, the entire html and the bundle is downloaded again and again everytime the user navigates from one page to another. To overcome
+        // this issue, we use <Link>. This reloads the page without reloading all its assets. Link prevents a full reload.
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">Navbar</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <NavLink className="nav-link" aria-current="page" to="/movies">Movies</NavLink>
+                        <NavLink className="nav-link" to="/customers">Customers</NavLink>
+                        <NavLink className="nav-link" to="/rentals">Rentals</NavLink>
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
-}
+};
 
 export default Navbar;
 
